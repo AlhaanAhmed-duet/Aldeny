7affcb7ce3e6112dba60d6eec8e8339990142f47
Danish2351 <dnsh2351@gmail.com>
2025-01-09 11:19:02 +0500

    Check

--- src/main/java/com/javaproject/aldeny/zip/maker/and/convertor/App.java
+++ src/main/java/com/javaproject/aldeny/zip/maker/and/convertor/App.java
@@ -1,8 +1,8 @@
 package com.javaproject.aldeny.zip.maker.and.convertor;
 
 import javafx.application.Application;
-import javafx.scene.StackPane;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
@@ -16,6 +16,7 @@ public class App extends Application {
         StackPane root = new StackPane();
         Scene scene = new Scene(root, 800, 640);
         stage.setScene(scene);
+        stage.setTitle("My Application");
         stage.show();
     }
 
